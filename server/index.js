0ded1be5fb42c63cd7eac63d3111c3d905de855d
Macintosh <rotema23@gmail.com>
2019-05-21 10:39:37 -0700

    with all of yesterdays changes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,29 @@ const massive = require("massive");
 const session = require("express-session");
 require("dotenv").config();
 
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+// server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on("connection", function(socket) {
+  socket.emit("news", { hello: "world" });
+  socket.on("my other event", function(data) {
+    console.log(data);
+  });
+});
+
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
 
 app.use(express.json());
 
 massive("db", db);
 
-app.listen(SERVER_PORT, () =>
+server.listen(SERVER_PORT, () =>
   console.log(SERVER_PORT, `server running on port ${SERVER_PORT}`)
 );
--- src/components/JoinGame/JoinGame.js
+++ src/components/JoinGame/JoinGame.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./JoinGame.scss";
 
 class JoinGame extends Component {
   render() {
