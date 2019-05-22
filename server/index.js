aed2fe41ea84a0c9102e71653a4f00287695317f
cam rains <csr66@nau.edu>
2019-05-22 10:49:44 -0700

    cams changes to add room dynamically

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
-const express = require("express");
-const app = express();
-const controller = require("./Controllers/controller");
-const massive = require("massive");
-const session = require("express-session");
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 require("dotenv").config();
+
+// ------------------------this is th eblock cam added in order to make socket rooms dynamically----
+// handle incoming connections from clients
+io.sockets.on('connection', function(socket) {
+  // once a client has connected, we expect to get a ping from them saying what room they want to join
+  socket.on('room', function(room) {
+    console.log(room)
+      socket.join(room.gameNumber);
+  });
+});
+// now, it's easy to send a message to just the clients in a given room
+room = "abc123";
+io.sockets.in(room).emit('message', 'what is going on, party people?');
+// this message will NOT go to the client defined above
+io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
+// -----------------------------------------------------------------
+
 
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
 
-app.use(express.json());
-
-massive("db", db);
-
-app.listen(SERVER_PORT, () =>
-  console.log(SERVER_PORT, `server running on port ${SERVER_PORT}`)
+const SocketController = require("./Controllers/SocketController");
+server.listen(SERVER_PORT, () =>
+  console.log(`server running on port ${SERVER_PORT}`)
 );
--- src/components/CreateGame/CreateGame.js
+++ src/components/CreateGame/CreateGame.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
-
+// set-up a connection between the client and the server
+import io from "socket.io-client";
+var socket = io.connect();
+// let's assume that the client page, once rendered, knows what room it wants to join
 class CreateGame extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gameNumber: null
+    };
+
+    // Connected, let's sign-up for to receive messages for this room
+
+    socket.on("message", function(data) {
+      console.log("Incoming message:", data);
+    });
+  }
+
+  generateRandom() {
+    this.setState({
+      gameNumber: Math.floor(100000 + Math.random() * 900000)
+    });
+  }
+
   render() {
-    return <div>This is CreateGame Component!</div>;
+    const { gameNumber } = this.state;
+    return (
+      <div>
+        Create Game Pin For New Game?
+        <button onClick={() => this.generateRandom()}>get pin</button>
+        <h3>{gameNumber}</h3>
+        <h3>Use this game number?</h3>
+        <button onClick={() => socket.emit("room", { gameNumber })}>Yes</button>
+        <button>No</button>
+        <h5>Share Game PIN with other players</h5>
+        <h3> Or join </h3>
+      </div>
+    );
   }
 }
-
 export default CreateGame;
