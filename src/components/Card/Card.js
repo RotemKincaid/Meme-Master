d323126ad39604b0c34c70eb6fd6c8e6766073e9
Macintosh <rotema23@gmail.com>
2019-05-27 16:53:25 -0700

    added styling mostly in createuser

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,13 +4,8 @@ import "./Card.scss";
 
 function Card(props) {
   return (
-    <div
-      key={props.card.card_id}
-      onClick={card => props.chooseCard(card)}
-      className="card"
-    >
+    <div onClick={() => props.chooseCard(props.card)} className="card">
       <p>{props.content}</p>
-      <button onClick={()=>props.chooseCard(props.card)}>CHOOSE CARD!</button>
     </div>
   );
 }
