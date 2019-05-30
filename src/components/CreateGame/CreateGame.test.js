385521a7b6d0065ade7b16ed4c77c19d5cad5523
Francisca <frastengo@gmail.com>
2019-05-30 12:06:16 -0700

    testingnotowrking

--- src/components/CreateGame/CreateGame.test.js
+++ src/components/CreateGame/CreateGame.test.js
@@ -1,28 +1,8 @@
-const createGame = require("./CreateGame.js")
-// import io from "socket.io-client";
-// const socket = io("http://localhost:4052");
+// const createGame = require("./CreateGame.js")
+// // import io from "socket.io-client";
+// // const socket = io("http://localhost:4052");
 
-describe("Create Game Container", () => {
-  describe("send game", () => {
-    
+// test("generateRandom() should set state with game pin", () => {
+//   expect(createGame.generateRandom() ).toEqual(this.setState())
+// })
 
-    // it("should emit the right game number", () => {
-    //   const gamepin = 16231;
-    //   socket.emit("1234", gamepin);
-
-    //   socket.on("4321", data);
-    //   expect(data).toEqual(16231);
-    // });
-
-    it("Should receive the game pin", () => {
-      
-    })
-
-
-
-
-
-
-
-  });
-});
